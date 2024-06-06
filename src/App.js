8a9565aba8f700bfbca2b9bcc0137a29e9287bc6
KipkorirNovestus <kipkorirnovestus1@gmail.com>
2024-06-06 22:28:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+function App() {
+ const ruto = <h1>This is Dr william ruto blog</h1>
+  return (
+    <>
+    <h1>William Kipchirchir Samoei Arap Ruto CGH</h1>
+    <p>William Kipchirchir Samoei Arap Ruto CGH (born 21 December 1966) is a Kenyan politician who is the fifth and current president of Kenya since 13 September 2022. Prior to becoming president, he served as the first elected deputy president of Kenya from 2013 to 2022. Previously, holders of the position were referred to as Vice President and the officeholder was unelected and appointed by the President.[1][2][3][4] He previously served in three cabinet portfolios as the Minister for Home Affairs, the Minister of Agriculture and as Minister for Higher Education.
+
+Ruto was elected Member of Parliament for Eldoret North constituency from 1997 to 2007 under the KANU, and from 2007 to 2013 through the ODM party. He was the Minister for Home Affairs in the Daniel arap Moi administration from August to December 2002. Under the Mwai Kibaki administration, he was the Minister for Agriculture from 2008 to 2010 and Minister for Higher Education from April to October 2010. Ruto first contested for the presidency during the 2007 election, but lost to Raila Odinga on the ODM party primaries; together with Musalia Mudavadi, who finished second, he then supported the candidature of Odinga. He again ran for the presidency on the 2013 election, but withdrew his candidature in favour of Uhuru Kenyatta. He was later nominated to run for the deputy presidency in the 2013 election under the United Republican Party, becoming the running mate of Uhuru Kenyatta from The National Alliance (TNA). He was re-elected to the deputy presidency under the Jubilee Party in the 2017 Kenyan general election. Ruto successfully ran for the presidency in the 2022 election, this time under the United Democratic Alliance (UDA). Amidst a fall-out, Kenyatta supported his opponent Raila Odinga. The election was marred by allegations of electoral fraud by allies of Odinga, although international observers have not corroborated such claims.[5][6]
+</p>
+ <h1> Early life and education</h1>
+  <p> A member of the Kalenjin people of the Rift Valley Province,[7] William Ruto was born on 21 December 1966 in Sambut village, Kamagut,[8] Uasin Gishu County, to Daniel Cheruiyot and Sarah Cheruiyot.[9]
+</p>
+
+ <h1>Education</h1>
+ <p> Ruto started his education at Kamagut Primary School, then transferred to Kerotet Primary School—both situated within the Uasin Gishu County; and he sat for his Certificate of Primary Education (CPE) at the latter. He then proceeded to Wareng Secondary School, still in Uasin Gishu County, and later Kapsabet High School in Nandi County, where he obtained his Ordinary Level and Advanced Level education respectively.[10]
+
+He then enrolled at the University of Nairobi to pursue Botany and Zoology, graduating in 1990 with a BSc.[11] He went on to complete an MSc in Plant Ecology, also from the University of Nairobi.[12] The year after his graduation, he enrolled for a Ph.D. at the same university, and after several setbacks,[13] he completed and graduated on 21 December 2018.
+
+Ruto authored several papers, including one titled Plant Species Diversity and Composition of Two Wetlands in the Nairobi National Park, Kenya.[14]
+
+During his time in the campus for his undergraduate education, Ruto was an active member of the Christian Union. He also served as the Chairman of the University of Nairobi's choir.[15] Through his church activities at the University of Nairobi, he met President Daniel arap Moi, who would later introduce him to politics during the 1992 general elections.[16]
+</p>
+Political career
+After graduating from the University of Nairobi in 1990, Ruto was employed as a temporary teacher in the North Rift region of Kenya from 1990 to 1992, where he was also a leader of the local church choir, the Africa Inland Church (AIC).[16]
+<h1>
+YK'92
+</h1>
+<p>
+Ruto began his political career when he became the treasurer of the YK'92 campaign group that was lobbying for the re-election of President Moi in 1992, from which he learned the basics of Kenyan politics.[15][17] He is also believed to have accumulated some wealth in this period.[18] After the 1992 elections, President Moi disbanded YK'92 and Ruto unsuccessfully vied for various branch-level positions in KANU, which was at that time Kenya's ruling party.[19]
+
+Member of Parliament
+Ruto ran for a parliamentary seat in the 1997 general election. He surprisingly beat the incumbent, Reuben Chesire, Moi's preferred candidate, as well as the Uasin Gishu KANU branch chairman and assistant minister.[20][21] He later gained favour with Moi and was appointed KANU Director of Elections.[22] His strong support in 2002 for Moi's preferred successor, Uhuru Kenyatta, saw him get a place as assistant minister in the Home Affairs (Interior) ministry. Later in that election, when some government ministers resigned to join the opposition, he was promoted to full Cabinet Minister.[18] KANU lost the election but he retained his parliamentary seat. Ruto was elected KANU Secretary General in 2005, with Uhuru Kenyatta elected as chairman.[22]
+</p> 
+    
+    
+    
+</>    
+    
+  );
+}
+
+export default App;
